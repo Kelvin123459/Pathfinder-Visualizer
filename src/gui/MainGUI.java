3d44a5c9935b84659536c5d3e74243c8784e1d97
kelvin123459 <kelvin.garcia5@upr.edu>
2021-01-30 15:52:29 -0400

    Updates to UI

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -3,6 +3,10 @@ package gui;
 import grid.GridMaker;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -11,7 +15,7 @@ import javafx.stage.Stage;
 public class MainGUI extends Application{
 	
 	double width = 800;
-	double height = 600;
+	double height = 675;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -22,8 +26,18 @@ public class MainGUI extends Application{
 		
 		VBox vbox2 = new VBox();
 		HBox hbox = new HBox();
+		Label alg = new Label("Algorithm: ");
+		MenuItem Astar = new MenuItem("A* Algorithm");
+		MenuItem Djikstra = new MenuItem("DJikstra");
+		MenuButton mbutton = new MenuButton("Choose an Algorithm...");
+		mbutton.getItems().add(Astar);
+		mbutton.getItems().add(Djikstra);
+		VBox vbox3 = new VBox();
+		Button start = new Button("Start");
+		Button stop = new Button("Stop");
 		
-		hbox.getChildren().addAll();
+		vbox3.getChildren().addAll(start, stop);
+		hbox.getChildren().addAll(alg, mbutton, vbox3);
 		vbox2.getChildren().addAll(hbox);
 		vbox.getChildren().addAll(grid, vbox2);
 		Scene scene = new Scene(vbox, width, height);
